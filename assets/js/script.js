dce88a040f565fdc69654896eef836cd7902fc11
Amr Sabry <95857185+Amr-Sabry1@users.noreply.github.com>
2023-08-14 10:41:13 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,94 @@
+'use strict';
+
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function(elem) { elem.classList.toggle("active"); }
+
+
+
+
+
+/**
+ * skills toggle
+ */
+
+const toggleBtnBox = document.querySelector("[data-toggle-box]");
+const toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+const skillsBox = document.querySelector("[data-skills-box]");
+
+for (let i = 0; i < toggleBtns.length; i++) {
+    toggleBtns[i].addEventListener("click", function() {
+
+        elemToggleFunc(toggleBtnBox);
+        for (let i = 0; i < toggleBtns.length; i++) { elemToggleFunc(toggleBtns[i]); }
+        elemToggleFunc(skillsBox);
+
+    });
+}
+
+
+
+/**
+ * dark & light theme toggle
+ */
+
+const themeToggleBtn = document.querySelector("[data-theme-btn]");
+
+themeToggleBtn.addEventListener("click", function() {
+
+    elemToggleFunc(themeToggleBtn);
+
+    if (themeToggleBtn.classList.contains("active")) {
+        document.body.classList.remove("dark_theme");
+        document.body.classList.add("light_theme");
+
+        localStorage.setItem("theme", "light_theme");
+    } else {
+        document.body.classList.add("dark_theme");
+        document.body.classList.remove("light_theme");
+
+        localStorage.setItem("theme", "dark_theme");
+    }
+
+});
+
+/**
+ * check & apply last time selected theme from localStorage
+ */
+
+if (localStorage.getItem("theme") === "light_theme") {
+    themeToggleBtn.classList.add("active");
+    document.body.classList.remove("dark_theme");
+    document.body.classList.add("light_theme");
+
+} else {
+    themeToggleBtn.classList.remove("active");
+    document.body.classList.remove("light_theme");
+    document.body.classList.add("dark_theme");
+}
+
+
+$(document).ready(function() {
+    $(".loader").fadeOut(4000, function() {
+        $(".spiner").fadeOut(500)
+        $("body").css("overflow", "auto")
+    });
+
+})
+
+
+let menuToggle = document.querySelector(".menuToggle");
+let sideBar = document.querySelector(".sidebar");
+menuToggle.onclick = function() {
+    sideBar.classList.toggle("active");
+};
+
+let Menulist = document.querySelectorAll(".Menulist li");
+
+function activeLink() {
+    Menulist.forEach((item) => item.classList.remove("active"));
+    this.classList.add("active");
+}
+Menulist.forEach((item) => item.addEventListener("click", activeLink));
