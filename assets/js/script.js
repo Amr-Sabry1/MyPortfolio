b34c0acf97297f526273b526310dbb036099756a
AmrSabryA <95857185+AmrSabryA@users.noreply.github.com>
2023-08-19 15:51:37 +0300

    edit some style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,12 @@
-'use strict';
+"use strict";
 
 /**
  * element toggle function
  */
 
-const elemToggleFunc = function(elem) { elem.classList.toggle("active"); }
-
-
-
-
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
 
 /**
  * skills toggle
@@ -19,16 +17,14 @@ const toggleBtns = document.querySelectorAll("[data-toggle-btn]");
 const skillsBox = document.querySelector("[data-skills-box]");
 
 for (let i = 0; i < toggleBtns.length; i++) {
-    toggleBtns[i].addEventListener("click", function() {
-
-        elemToggleFunc(toggleBtnBox);
-        for (let i = 0; i < toggleBtns.length; i++) { elemToggleFunc(toggleBtns[i]); }
-        elemToggleFunc(skillsBox);
-
-    });
+  toggleBtns[i].addEventListener("click", function () {
+    elemToggleFunc(toggleBtnBox);
+    for (let i = 0; i < toggleBtns.length; i++) {
+      elemToggleFunc(toggleBtns[i]);
+    }
+    elemToggleFunc(skillsBox);
+  });
 }
-
-
 
 /**
  * dark & light theme toggle
@@ -36,22 +32,20 @@ for (let i = 0; i < toggleBtns.length; i++) {
 
 const themeToggleBtn = document.querySelector("[data-theme-btn]");
 
-themeToggleBtn.addEventListener("click", function() {
+themeToggleBtn.addEventListener("click", function () {
+  elemToggleFunc(themeToggleBtn);
 
-    elemToggleFunc(themeToggleBtn);
+  if (themeToggleBtn.classList.contains("active")) {
+    document.body.classList.remove("dark_theme");
+    document.body.classList.add("light_theme");
 
-    if (themeToggleBtn.classList.contains("active")) {
-        document.body.classList.remove("dark_theme");
-        document.body.classList.add("light_theme");
+    localStorage.setItem("theme", "light_theme");
+  } else {
+    document.body.classList.add("dark_theme");
+    document.body.classList.remove("light_theme");
 
-        localStorage.setItem("theme", "light_theme");
-    } else {
-        document.body.classList.add("dark_theme");
-        document.body.classList.remove("light_theme");
-
-        localStorage.setItem("theme", "dark_theme");
-    }
-
+    localStorage.setItem("theme", "dark_theme");
+  }
 });
 
 /**
@@ -59,36 +53,32 @@ themeToggleBtn.addEventListener("click", function() {
  */
 
 if (localStorage.getItem("theme") === "light_theme") {
-    themeToggleBtn.classList.add("active");
-    document.body.classList.remove("dark_theme");
-    document.body.classList.add("light_theme");
-
+  themeToggleBtn.classList.add("active");
+  document.body.classList.remove("dark_theme");
+  document.body.classList.add("light_theme");
 } else {
-    themeToggleBtn.classList.remove("active");
-    document.body.classList.remove("light_theme");
-    document.body.classList.add("dark_theme");
+  themeToggleBtn.classList.remove("active");
+  document.body.classList.remove("light_theme");
+  document.body.classList.add("dark_theme");
 }
 
-
-$(document).ready(function() {
-    $(".loader").fadeOut(4000, function() {
-        $(".spiner").fadeOut(500)
-        $("body").css("overflow", "auto")
-    });
-
-})
-
+$(document).ready(function () {
+  $(".loader").fadeOut(5000, function () {
+    $(".spiner").fadeOut(500);
+    $("body").css("overflow", "auto");
+  });
+});
 
 let menuToggle = document.querySelector(".menuToggle");
 let sideBar = document.querySelector(".sidebar");
-menuToggle.onclick = function() {
-    sideBar.classList.toggle("active");
+menuToggle.onclick = function () {
+  sideBar.classList.toggle("active");
 };
 
 let Menulist = document.querySelectorAll(".Menulist li");
 
 function activeLink() {
-    Menulist.forEach((item) => item.classList.remove("active"));
-    this.classList.add("active");
+  Menulist.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
 }
 Menulist.forEach((item) => item.addEventListener("click", activeLink));
